4e0139b571a6a75ce79b6bdf298064046d86625c
longqi <714639106@qq.com>
2017-02-06 14:40:27 +0800

    changes log

---
+++ flask/flask.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+# 导入所有的模块
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# 配置文件
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+if __name__ == '__main__':
+    app.run()
